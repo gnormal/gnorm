4856092e4e02525d5a5c3a536f0a35817b17b686
gernest <geofreyernest@live.com>
2017-09-15 12:35:29 +0300

    Delete drivers/driver.go

--- database/drivers/driver.go
+++
@@ -1,8 +0,0 @@
-package drivers
-
-// Templates is an interface for gnorm templates. The templates vary depending
-// on the active driver.
-type Templates interface {
-	TplNames() []string
-	Tpl(name string) ([]byte, error)
-}
