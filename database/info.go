e07e6516b6fbcf417c6caea105c277f2f2e56fcc
gernest <geofreyernest@live.com>
2017-09-15 14:25:28 +0300

    Add Strings.Join
    I noticed  TestStringsJoin was failing because there was no Join method on Strings.

--- database/info.go
+++ database/info.go
@@ -1,6 +1,7 @@
 package database // import "gnorm.org/gnorm/database"
 import (
 	"fmt"
+	"strings"
 )
 
 // Info is the collection of schema info from a database.
@@ -110,4 +111,9 @@ func (s Strings) Sprintf(format string) Strings {
 		ret[x] = fmt.Sprintf(format, s[x])
 	}
 	return ret
+}
+
+// Join uses strings.Join to join the string values with separator sep.
+func (s Strings) Join(sep string) string {
+	return strings.Join(s, sep)
 }
